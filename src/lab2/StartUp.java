46f62c30787e40ee904f126a981266be0aae393c
L115student <L115student@L115-08-043627.bitnet.wctc.edu>
2017-09-19 12:27:44 -0500

    Finished Lab 2 Documentation

--- src/lab2/StartUp.java
+++ src/lab2/StartUp.java
@@ -20,7 +20,8 @@ public class StartUp {
         extend the IntroToJavaCourse from intro to programming and add the prerequisites, and then extend
         AdvancedJava from IntroToJava. The problem with the Liskov Substitution principle there would be I 
         couldnt create a IntroToProgrammingCourse and then assign a new IntroToJava course because
-        IntroToJava "isn't a" IntroToProgrammingCourse.
+        IntroToJava "isn't a" IntroToProgrammingCourse. It needs all the same properties but isn't the right
+        abstractions.
         */
         
         lab2.SchoolCourse course1 = new lab2.IntroToProgrammingCourse("Intro to Programming", "C102");
